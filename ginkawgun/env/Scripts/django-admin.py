95c009d8a296f2f5998828353bfa93e720e718a0
Nuntavat suthangkornkul <42973803+Nun0289@users.noreply.github.com>
2020-04-02 00:45:26 +0700

    .

--- ginkawgun/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\jaroo\documents\github\webpro_finalproject\ginkawgun\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
