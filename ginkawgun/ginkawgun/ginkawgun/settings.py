3b7e009886dab39b72bb07baf81673735bca1f98
Nuntavat suthangkornkul <42973803+Nun0289@users.noreply.github.com>
2020-04-12 22:25:55 +0700

    .

--- ginkawgun/ginkawgun/ginkawgun/settings.py
+++ ginkawgun/ginkawgun/ginkawgun/settings.py
@@ -74,11 +74,15 @@ WSGI_APPLICATION = 'ginkawgun.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+                'default': {
+                    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                    'NAME': 'postgres',
+                    'USER': 'postgres',
+                    'PASSWORD': '1234',
+                    'HOST': 'localhost',
+                    'PORT': '5432',
+                }
+            }
 
 
 # Password validation
