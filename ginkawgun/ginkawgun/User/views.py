61bcb861e884e895d01e9059671ddad7d4cafb73
Nuntavat suthangkornkul <42973803+Nun0289@users.noreply.github.com>
2020-04-17 23:47:14 +0700

    .

--- ginkawgun/ginkawgun/User/views.py
+++ ginkawgun/ginkawgun/User/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def app_login(request):
     return render(request, template_name='login.html')
+def app_register(request):
+    return render(request, template_name='register.html')
--- ginkawgun/ginkawgun/ginkawgun/urls.py
+++ ginkawgun/ginkawgun/ginkawgun/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from User import views as user_views
 from Manager import views as manager_views
 urlpatterns = [
-    path('login/',user_views.app_login,name='login')
+    path('login/',user_views.app_login,name='login'),
+    path('register/',user_views.app_register,name='register')
     ,path('admin/', admin.site.urls ,name='admin')
     ,path('',manager_views.app_homepage, name='homepage')
 ]
