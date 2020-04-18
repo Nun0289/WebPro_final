7a0a51ff9160f045bb9923a5940e410f26957aae
Nuntavat suthangkornkul <42973803+Nun0289@users.noreply.github.com>
2020-04-18 14:44:02 +0700

    .

--- ginkawgun/ginkawgun/User/views.py
+++ ginkawgun/ginkawgun/User/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+
 # Create your views here.
 def app_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request,user)
+            return redirect('homepage')
+        else:
+            context['username'] = username
+            context['password'] = password
+            context['error'] = 'Wrong username or password'
+
     return render(request, template_name='login.html')
 def app_register(request):
     return render(request, template_name='register.html')
