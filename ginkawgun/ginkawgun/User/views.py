0025ad979f809e88f24d45d2fe4c2d6bf4fabc8f
Nuntavat suthangkornkul <42973803+Nun0289@users.noreply.github.com>
2020-04-21 03:04:27 +0700

    .

--- ginkawgun/ginkawgun/User/views.py
+++ ginkawgun/ginkawgun/User/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 
-# Create your views here.
+
 def app_login(request):
     context = {}
     if request.method == 'POST':
